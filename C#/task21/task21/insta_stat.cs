b45cbaa50001a798178d211954c5797289574f52
rojakh <rojakh153@gmail.com>
2022-04-17 19:28:20 +0530

    commit

---
+++ C#/task21/task21/insta_stat.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task21
+{
+    internal class cycle
+    {
+    
+        
+            //Static method or class method
+            public static void run()
+            {
+                Console.WriteLine("This is the static method");
+            }
+
+            //instanse method
+            public void engine()
+            {
+                Console.WriteLine("This is the instance method");
+            }
+
+        }
+
+
+        class Program
+        {
+            static void Main(string[] args)
+            {
+                //Call static method by class name 
+                cycle.run();
+
+
+                //Call non-static method. call method by object
+                cycle c = new cycle();
+                c.engine();
+            }
+        }
+}
+
