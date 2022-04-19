93b84ec43d6468870c60823d87423f132c6f3117
rojakh <rojakh153@gmail.com>
2022-04-19 11:26:25 +0530

    commit

---
+++ C#/task_22/task_22/string_builder.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_22
+{
+    internal class string_builder
+    {
+        public static void Main()
+        {
+            //creation of StringBuilder
+            StringBuilder sb = new StringBuilder("Hello", 50);
+
+            //Append method
+            sb.Append("Hello ");
+            sb.AppendLine("World!");
+            sb.AppendLine("Hello C#");
+            Console.WriteLine(sb);
+
+            //Insert method
+            sb.Insert(10, "roja");
+            Console.WriteLine(sb);
+
+            //Remove method
+            sb.Remove(5, 7);
+            Console.WriteLine(sb);
+
+            //Replace method
+            sb.Replace("Hello", "haiii");
+            Console.WriteLine(sb);
+        }
+    }
+}
