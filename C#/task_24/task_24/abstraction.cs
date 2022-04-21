83be4af9cfdd1f62b555d11b3c778c88a1b984db
rojakh <rojakh153@gmail.com>
2022-04-21 12:51:26 +0530

    commit

---
+++ C#/task_24/task_24/abstraction.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_24
+{
+    public abstract class Plan
+    {
+        protected double rate;
+        public abstract void getRate();
+        public void calculation(int units)
+        {
+            Console.Write("BILL AMOUNT FOR " + units + " UNITS is: Rs.");
+            Console.WriteLine(rate * units);
+        }
+    }
+    class VillagePlan : Plan
+    {
+        public override void getRate()
+        {
+            rate = 5.00;
+        }
+    }
+    class CityPlan : Plan
+    {
+        public override void getRate()
+        {
+            rate = 2.50;
+        }
+    }
+    internal class Abstraction
+    {
+        static void Main()
+        {
+            Plan p;
+            Console.WriteLine("VILLAGE CONNECTION");
+            p = new VillagePlan();
+            p.getRate();
+            p.calculation(550);
+            Console.WriteLine("CITY CONNECTION");
+            p = new CityPlan();
+            p.getRate();
+            p.calculation(250);
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ C#/task_24/task_24/interface.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace task_24
+{
+    internal class area_peri
+    {
+        interface area
+        {
+            double res(int w, int h);
+            
+        }
+        interface perimeter
+        {
+            int res2(int a, int b, int c);
+        }
+        class Calculation : area , perimeter
+        {
+            public double result1;
+            public double res(int w, int h)
+            {
+                return result1 =0.5 * w * h;
+            }
+            public int result2;
+            public int res2(int a, int b,int c)
+            {
+                return result2 = a + b + c;
+            }
+           internal class Program
+            {
+                static void Main()
+                {
+                    Calculation c = new Calculation();
+                    c.res(8, 2);
+                    c.res2(20, 18, 45);
+                    Console.WriteLine("Area of triangle is " + c.result1);
+                    Console.WriteLine("Perimeter of triangle is " + c.result2);
+                }
+            }
+        }
+    }
+}
